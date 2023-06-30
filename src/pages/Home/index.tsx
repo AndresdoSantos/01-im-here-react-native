02596c84263044ba496c5c8ef5a2813129428e50
Desenvolvimento <desenvolvimento@zaal.com.br>
2023-06-30 09:59:15 -0300

    feat: create home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,25 @@
+import { Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { styles } from './styles'
+
+export function Home() {
+  function handleAddParticipant() {}
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Nome do evento</Text>
+      <Text style={styles.subtitle}>Sexta, 4 de fevereiro de 2023</Text>
+
+      <View style={styles.form}>
+        <TextInput
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6B"
+          style={styles.input}
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handleAddParticipant}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,54 @@
+import { StyleSheet } from 'react-native'
+
+const COLOR = {
+  WHITE: '#FDFCFE',
+  GREEN: '#31CF65',
+  GRAY: '#1F1E25',
+  GRAY_LIGHT: '#6B6B6B',
+}
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#131016',
+    padding: 24,
+  },
+  title: {
+    color: COLOR.WHITE,
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48,
+  },
+  subtitle: {
+    color: COLOR.GRAY_LIGHT,
+    fontSize: 16,
+  },
+  form: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 12,
+    marginTop: 36,
+    marginBottom: 42,
+  },
+  input: {
+    backgroundColor: COLOR.GRAY,
+    height: 56,
+    flex: 1,
+    borderRadius: 5,
+    padding: 16,
+    color: COLOR.WHITE,
+    fontSize: 16,
+  },
+  button: {
+    height: 56,
+    width: 56,
+    borderRadius: 5,
+    backgroundColor: COLOR.GREEN,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: COLOR.WHITE,
+    fontSize: 24,
+  },
+})
